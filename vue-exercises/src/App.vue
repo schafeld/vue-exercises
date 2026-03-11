3e4eb5808320afe084fdc5b15f5f9c9811da9a99
Oliver Schafeld <oliver@schafeld.com>
2026-03-11 10:11:45 +0100

    add custom component

--- vue-exercises/src/App.vue
+++ vue-exercises/src/App.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import MyComponent from './components/MyComponent.vue';
+</script>
 
 <template>
   <h1>😃 I did it!</h1>
@@ -6,6 +8,7 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <MyComponent prop="Hello Prop!" />
 </template>
 
 <style scoped></style>
---
+++ vue-exercises/src/components/MyComponent.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  defineProps<{
+    prop: string;
+  }>();
+
+  function getCharacteristices(value: string) {
+    return `${value.length} characters and the first character is ${value[0]}`;
+  }
+
+</script>
+
+<template>
+  <h2>This is MyComponent</h2>
+  <p>This is a prop: {{ prop }}</p>
+  <p>It is a string that has: {{ getCharacteristices(prop) }}.</p>
+</template>
+
+<style scoped></style>
